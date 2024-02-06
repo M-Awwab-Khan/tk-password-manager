560a7e1bfd5457b0695a6376da48816a5bfcefda
Awwab <mawwabkhank2006@gmail.com>
2024-02-06 11:54:06 +0500

    added padx to improve ui

--- main.py
+++ main.py
@@ -15,15 +15,15 @@ logo = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=logo)
 canvas.grid(row=0, column=1)
 
-ttk.Label(text='Website: ').grid(row=1, column=0)
-ttk.Label(text='Email/Username: ').grid(row=2, column=0)
-ttk.Label(text='Password: ').grid(row=3, column=0)
+ttk.Label(text='Website: ').grid(row=1, column=0, pady=5)
+ttk.Label(text='Email/Username: ').grid(row=2, column=0, pady=5)
+ttk.Label(text='Password: ').grid(row=3, column=0, pady=5)
 
-website = ttk.Entry(width=42).grid(row=1, column=1, columnspan=2)
-email = ttk.Entry(width=42).grid(row=2, column=1, columnspan=2)
-password = ttk.Entry(width=23).grid(row=3, column=1)
+website = ttk.Entry(width=42).grid(row=1, column=1, columnspan=2, pady=5)
+email = ttk.Entry(width=42).grid(row=2, column=1, columnspan=2, pady=5)
+password = ttk.Entry(width=23).grid(row=3, column=1, pady=5)
 
-add = ttk.Button(text='Add', width=42).grid(row=4, column=1, columnspan=2)
+add = ttk.Button(text='Add', width=42).grid(row=4, column=1, columnspan=2, pady=5)
 gen_pass = ttk.Button(text='Generate Password').grid(row=3, column=2)
 
 sv_ttk.set_theme('light')
