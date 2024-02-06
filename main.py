182d274702c4b2348be349598a8ffb2e2699c444
Awwab <mawwabkhank2006@gmail.com>
2024-02-06 11:50:29 +0500

    UI setup

--- main.py
+++ main.py
@@ -6,11 +6,22 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Password Manager')
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(height=200, width=200)
 logo = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=logo)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+Label(text='Website: ').grid(row=1, column=0)
+Label(text='Email/Username: ').grid(row=2, column=0)
+Label(text='Password: ').grid(row=3, column=0)
+
+website = Entry(width=35).grid(row=1, column=1, columnspan=2)
+email = Entry(width=35).grid(row=2, column=1, columnspan=2)
+password = Entry(width=21).grid(row=3, column=1)
+
+add = Button(text='Add', width=36).grid(row=4, column=1, columnspan=2)
+gen_pass = Button(text='Generate Password').grid(row=3, column=2)
 
 window.mainloop()
